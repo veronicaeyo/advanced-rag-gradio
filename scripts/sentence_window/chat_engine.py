8d5a784898536ba3818bd3290e7d863b1faa6b0b
Veronica Eyo <ve7974293@gmail.comgit          global user.email ve7974293@gmail.com>
2024-02-21 12:45:48 -0500

    changed chat engine in sentence_window

--- scripts/sentence_window/chat_engine.py
+++ scripts/sentence_window/chat_engine.py
@@ -6,20 +6,29 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.indices.base import BaseIndex
 from llama_index.core.query_engine import BaseQueryEngine
 
+from scripts.utils import memory
+from llama_index.core.chat_engine.types import ChatMode, BaseChatEngine
+from llama_index.core.llms.utils import LLMType
+
 
 def build_sentence_window_chat_engine(
+    llm: LLMType, 
     index: VectorStoreIndex | BaseIndex,
     similarity_top_k=6,
     rerank_top_n=2,
-) -> BaseQueryEngine:
+) -> BaseChatEngine:
     # define postprocessors
     postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
     rerank = SentenceTransformerRerank(
         top_n=rerank_top_n, model="cross-encoder/ms-marco-TinyBERT-L-2-v2"
     )
 
-    sentence_window_engine = index.as_query_engine(
+    sentence_window_engine = index.as_chat_engine(
+        llm=llm,
+        chat_mode=ChatMode.CONDENSE_PLUS_CONTEXT,
+        verbose=True,
         similarity_top_k=similarity_top_k,
+        memory=memory,
         node_postprocessors=[postproc, rerank],
         streaming=True,
     )
